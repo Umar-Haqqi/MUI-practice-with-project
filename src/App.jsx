c139c1526951d34af4ea385999c6fc2fd1f096c7
Umar-Haqqi <umer1562001@gmail.com>
2023-12-28 16:03:26 +0500

    MUI custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { Settings, Add, Delete } from '@mui/icons-material';
 function App() {
 
   // making button component for reusablity
-  const BlueButton = styled(Button)({
-    backgroundColor: 'skyblue',
-    color: '#888',
+  const BlueButton = styled(Button)(({ theme }) => ({
+
+    // giving theme color here
+    backgroundColor: theme.palette.other.main,
+    color: theme.palette.primary.light,
+
     margin: 5,
     '&:hover': {
       backgroundColor: 'lightblue',
     },
-  })
+  }));
 
   return (
     <>
+
       {/* Custom button with MUI */}
       <BlueButton startIcon={<Add />}>Custom button</BlueButton>
       <BlueButton startIcon={<Delete />}>Another button</BlueButton>
---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3f51b5',
+            light: '#ff1a8c',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        other: {
+            main: '#999',
+        }
+    }
+})
