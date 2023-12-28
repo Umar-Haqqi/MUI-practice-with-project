ff037304edbebe0279f2914e64472d49138ae06f
Umar-Haqqi <umer1562001@gmail.com>
2023-12-28 15:39:38 +0500

    custom Button component with MUI for reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
-import { Button, Typography } from '@mui/material'
-// import Setting from '@mui/icons-material/Settings';
-// import Add from '@mui/icons-material/Add';
-
+import { Button, styled } from '@mui/material'
 import { Settings, Add, Delete } from '@mui/icons-material';
 
 function App() {
 
+  // making button component for reusablity
+  const BlueButton = styled(Button)({
+    backgroundColor: 'skyblue',
+    color: '#888',
+    margin: 5,
+    '&:hover': {
+      backgroundColor: 'lightblue',
+    },
+  })
+
   return (
     <>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained" color="secondary">Settings</Button>
-      <Button startIcon={<Add />} variant="contained" color="success">Add</Button>
-      <Button startIcon={<Delete />} variant="contained" color="secondary" disabled>Delete</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <Typography marginY={2} variant="h1" bgcolor={"grey.300"} component="p">
-        it uses h1 styles but its a p tag
-      </Typography>
-
-      {/* unique button */}
-      <Button variant='contained'
-        sx={
-          {
-            backgroundColor: 'skyblue',
-            color: '#888',
-            margin: 1,
-            '&:hover': {
-              backgroundColor: 'lightblue',
-              
-            },
-          }
-        }>Custom Button
-      </Button>
+      {/* Custom button with MUI */}
+      <BlueButton startIcon={<Add />}>Custom button</BlueButton>
+      <BlueButton startIcon={<Delete />}>Another button</BlueButton>
+      <BlueButton startIcon={<Settings />}>Another button</BlueButton>
     </>
   )
 }
